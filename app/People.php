2cc6eaaf6145c57021e1e58bf66c9d5d07d65bb8
nazar-kushniruk <kushniruknazar@gmail.com>
2019-02-03 14:45:01 +0200

    add migration, create several elements

---
+++ app/People.php
@@ -0,0 +1,31 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Запустить миграции.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('twitter');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Откат миграций.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
